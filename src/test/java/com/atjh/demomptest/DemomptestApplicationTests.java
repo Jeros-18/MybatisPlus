c9120e145f036895cb5db7cec44526ed95f3943b
小星尘 <jahui6690429@163.com>
2021-07-07 22:11:53 +0800

    08简单查询

--- src/test/java/com/atjh/demomptest/DemomptestApplicationTests.java
+++ src/test/java/com/atjh/demomptest/DemomptestApplicationTests.java
@@ -2,11 +2,14 @@ package com.atjh.demomptest;
 
 import com.atjh.demomptest.entity.User;
 import com.atjh.demomptest.mapper.UserMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 @SpringBootTest
@@ -15,6 +18,44 @@ class DemomptestApplicationTests {
 
     @Autowired
     private UserMapper userMapper;
+
+    //分页查询
+    @Test
+    public void testSelectPage(){
+        Page<User> page = new Page(1, 3);
+        Page<User> userPage = userMapper.selectPage(page, null);
+        //返回对象得到分页所有数据
+        long pages = userPage.getPages(); //总页数
+        long current = userPage.getCurrent(); //当前页
+        List<User> records = userPage.getRecords(); //查询数据集合
+        long total = userPage.getTotal(); //获取总记录数
+        boolean hasNext = userPage.hasNext(); //下一页
+        boolean hasPrevious = userPage.hasPrevious(); //上一页
+
+        System.out.println(pages);
+        System.out.println(current);
+        System.out.println(records);
+        System.out.println(total);
+        System.out.println(hasNext);
+        System.out.println(hasPrevious);
+    }
+
+    //简单条件查询
+    @Test
+    public void testSelect2(){
+        HashMap<String, Object> columMap = new HashMap<>();
+        columMap.put("name","Jone");
+        columMap.put("age",18);
+        List<User> users = userMapper.selectByMap(columMap);
+        System.out.println(users);
+    }
+
+    //多个Id批量查询
+    @Test
+    public void testSelect(){
+        List<User> users = userMapper.selectBatchIds(Arrays.asList(1, 2, 3));
+        System.out.println(users);
+    }
 
     //测试乐观锁
     @Test
