13df51aef566ba664546e40f76a4c871ddc9b150
小星尘 <jahui6690429@163.com>
2021-07-07 19:04:30 +0800

    实现增、改、自动填充

--- src/main/java/com/atjh/demomptest/entity/User.java
+++ src/main/java/com/atjh/demomptest/entity/User.java
@@ -1,6 +1,12 @@
 package com.atjh.demomptest.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class User {
@@ -8,4 +14,10 @@ public class User {
     private String name;
     private Integer age;
     private String email;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;  //create_time
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime; //update_time
 }
--- src/test/java/com/atjh/demomptest/DemomptestApplicationTests.java
+++ src/test/java/com/atjh/demomptest/DemomptestApplicationTests.java
@@ -16,6 +16,30 @@ class DemomptestApplicationTests {
     @Autowired
     private UserMapper userMapper;
 
+    //修改
+    @Test
+    public void testUpdate(){
+        User user = new User();
+        user.setId(1412697892593876993L);
+        user.setName("lucyMaaaa");
+        int count = userMapper.updateById(user);
+        System.out.println(count);
+
+    }
+
+
+    //添加
+    @Test
+    public void testAdd() {
+        User user = new User();
+        user.setName("lucyeee");
+        user.setAge(20);
+        user.setEmail("1243@qq.com");
+        int insert = userMapper.insert(user);
+        System.out.println(insert);
+    }
+
+    //查找全部
     @Test
     public void findAll(){
         List<User> users = userMapper.selectList(null);
